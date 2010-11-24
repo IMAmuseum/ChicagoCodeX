85e1a0847da24b1c8b11f6fea3d12a939bea4362
karnold <karnold@imamuseum.org>
2010-11-24 17:54:07 +0000

    EOD commit
    git-svn-id: http://athena.imamuseum.org/svn/ima/trunk/labs/osci/default@15258 a4814893-3c26-0410-bcaf-a94346362ac2

--- modules/osci/js/body_copy_field.js
+++ modules/osci/js/body_copy_field.js
@@ -1,37 +1,43 @@
+var newIdSelector = '';
 (function ($) {
     $(document).ready(function() {
-        
+        $('.footnotes-wrapper .fieldset-wrapper').tabs({
+            add : function (e, ui) {
+                $(this).tabs("select", ui.panel.id);
+            },
+            selected : 1
+        });
 
         $("<a />", {
             "class" : "footnote-remove",
             text : "remove",
-            "src" : "#",
-            "click" : function(e){
-                e.preventDefault();
-                $(this).parent().parent().remove();
-            }
+            "src" : "#"
         }).appendTo($("label",".footnotes-wrapper"));
+
+        $('div[id$="footnote-blank"]').hide();
+        $("li:first", "ul.ui-tabs-nav").hide();
+
+        $(".footnote-remove").live("click", function(e) {
+            e.preventDefault();
+            //$("textarea", $(this).parent().parent()).remove();
+            var $tabs = $(this).parents(".ui-tabs");
+            $tabs.tabs("remove", $tabs.tabs('option', 'selected'));
+        });
 
         $("a.footnote-add-another").click(function(e){
             e.preventDefault();
-            var container = $(this).parents("div.fieldset-wrapper:first");
+            var container = $(this).parents("div.ui-tabs");
             var hiddenCountElem = $('[type="hidden"]', container);
             var count = parseFloat(hiddenCountElem.val()) + 1;
-            var newElement = $('.form-item:first', container).clone();
-            var replaceText = $("label", newElement).html().trim(); 
-            var newText = replaceText.substr(0, replaceText.lastIndexOf("-") + 1) + count;
-console.log(newElement);
-console.log(replaceText);
-console.log(newText);
+            var newElement = $('div[id$="footnote-blank"]', container).clone().show();
 
-            var newHtml = newElement.wrap("<span>").parent().html();
-            newHtml = newHtml.replace(new RegExp(replaceText, "gi"), newText);
+            var newHtml = newElement.wrap("<span />").parent().html();
+            newHtml = newHtml.replace(/footnote-blank/gi, count);
+            newIdSelector = "#" + $(newHtml).attr("id");
 
-            replaceText = replaceText.replace("_", "-");
-            newText = newText.replace("_", "-");
-            newHtml = newHtml.replace(new RegExp(replaceText, "gi"), newText);
+            container.append(newHtml);
 
-            $(this).parent().before(newHtml);
+            container.tabs("add", newIdSelector, count);
             hiddenCountElem.val(count);
         });
     });
--- modules/osci/js/osci_filters.js
+++ modules/osci/js/osci_filters.js
@@ -1,29 +1,50 @@
 (function ($) {
-    $(document).ready(function() {
-
 /*
-CKEDITOR.plugin.add('osci_filters', {
-    init:   function(editor) {
-        editor.ui.addButton('footnote', {
-            label: 'Footnote',
-            icon: this.path + 'footnote_edit.png',
-            command: b
-        });
-    }
-});
+    CKEDITOR.plugin.add('osci_filters', {
+        init:   function(editor) {
+            editor.ui.addCommand('addfootnote', new CKEDITOR.dialogCommand('addfootnote');
+            editor.ui.addButton('footnote', {
+                label: 'Footnote',
+                icon: this.path + 'footnote_edit.png',
+                command: 'addfootnote' 
+            });
+        }
+    });
 */
-        var editor = CKEDITOR.instances[Drupal.wysiwyg.activeId];
 
-        editor.on('paste', function(evt) {
-            var data    = $('<span>' + evt.data.html + '</span>');
-            var anchors = [];
-            $('a', data).each(function(idx, val) {
-                anchors[idx] = val;
-                var link = $(val);
-                link.replaceWith('[footnote:' + idx + ']');
-            }); 
-            evt.data.html = data.html();
+    $(document).ready(function() {
+        $('.field-type-osci-body-copy').each(function() {
+            var id = $(this).attr('id');
+            listenForPaste($('#' + id + ' textarea').attr('id'));
         });
+
+        function listenForPaste(selector) {
+            var editor = CKEDITOR.instances[selector];
+
+            editor.on('paste', function(evt) {
+                //  replace footnotes with data
+                var data = $('<span>' + evt.data.html + '</span>');
+                // Search for footnote links in body text
+                $('a', data).each(function(idx, val) {
+                    var name = $(this).attr('name');
+                    var id = idx + 1;
+
+                    if (name.indexOf('_ftnref') === 0) {
+                        $('#' + selector).parents('.fieldset-wrapper').find('.footnote-add-another').click();
+                        var footnote = name.replace('ref', '');
+                        var content  = data.find('[name=' + footnote + ']').next().html();
+
+                        $(val).replaceWith('[footnote:' + newIdSelector + ']');
+                        $(newIdSelector.replace('#','') + ' textarea').html(content);
+                    } else {
+                        $(this).next().html('');
+                        $(this).html('');
+                    }
+                }); 
+
+                evt.data.html = data.html();
+            });
+        }
 
     });
 })(jQuery);
