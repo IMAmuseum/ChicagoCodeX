b9803746692428fb167a20920da4d7ba3ce7a896
karnold <karnold@imamuseum.org>
2011-02-03 19:57:51 +0000

    implemented footnote wysiwyg plugin
    git-svn-id: http://athena.imamuseum.org/svn/ima/trunk/labs/osci/default@15843 a4814893-3c26-0410-bcaf-a94346362ac2

--- modules/osci/js/dialogs/footnote.js
+++ modules/osci/js/dialogs/footnote.js
@@ -1,28 +1,43 @@
-( function() {
+( function($) {
     function footnoteDialog(editor) {
-        var buttons = [{
-            type:       'button',
-            id:         'newFootnote',
-            label:      'New Footnote',
-            onClick:    footnoteNew 
+        var buttons = [ CKEDITOR.dialog.okButton, CKEDITOR.dialog.cancelButton ];
+        var footnotes = $('#' + editor.name)
+            .parents('.field-type-osci-body-copy')
+            .find('.footnote-wrapper:not(:last)');
+        var items = [['', '']];
+    
+        footnotes.each(function(idx) {
+            items.push([(idx + 1) + ' (' + $(this).attr('id') + ')', $(this).attr('id')]);
+        });
 
-        }, CKEDITOR.dialog.okButton, CKEDITOR.dialog.cancelButton]
+        var elements = [{
+            type:           'text',
+            id:             'footnoteId',
+            label:          'Enter a footnote ID',
+            labelLayout:    'horizontal'
+        },
+        {
+            type:           'html',
+            html:           '<b>OR</b>'
+        },
+        {
+            type:           'select',
+            id:             'existingFootnote',
+            items:          items,
+            label:          'Select an existing footnote',
+            labelLayout:    'horizontal'
+        }];
 
         var contents = [{
             id:     'footnote',
             label:  'Add footnote',
-            elements:   [{
-                type:           'text',
-                id:             'addFootnote',
-                label:          'Footnote ID',
-                labelLayout:    'horizontal'
-            }]
+            elements:   elements
         }] 
         
         return {
             title: 'Add a footnote',
             minWidth:   400,
-            minHeight:  300,
+            minHeight:  100,
             buttons:    buttons,
             resizable:  'none',
             contents:   contents,
@@ -31,14 +46,21 @@
     }
 
     function footnoteOk(data) {
-console.log(data);
-    }
+        var editor = this.getParentEditor();
+        var dialog = CKEDITOR.dialog.getCurrent();
+        var footnoteId = dialog.getValueOf('footnote', 'footnoteId');
+        var existingFootnote = dialog.getValueOf('footnote', 'existingFootnote');
+    
+        if (existingFootnote != '') {
+            var replace = existingFootnote;
+        } else if (footnoteId != '') {
+            var replace = footnoteId;
+        }
 
-    function footnoteNew(data) {
-console.log(data);
+        editor.insertText('[footnote:' + replace + ']');
     }
 
     CKEDITOR.dialog.add('footnote', function(editor) {
         return footnoteDialog(editor);
     });
-})();
+})(jQuery);
