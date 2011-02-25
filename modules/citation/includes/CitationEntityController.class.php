6a9c46a434d8f6e1947f85d0776998902bd69b0f
karnold <karnold@imamuseum.org>
2011-02-25 21:23:30 +0000

    initial import of citation module
    git-svn-id: http://athena.imamuseum.org/svn/ima/trunk/labs/osci/default@16184 a4814893-3c26-0410-bcaf-a94346362ac2

---
+++ modules/citation/includes/CitationEntityController.class.php
@@ -0,0 +1,31 @@
+<?php
+
+/***********************************************
+ * Class to provide CRUD functions for citation entity
+ */
+class CitationEntityController extends DrupalDefaultEntityController {
+
+    public function insert() {
+
+    }
+
+    public function update($entity) {
+
+    }
+
+    public function load($entity) {
+
+    }
+
+    public function view($entity) {
+
+    }
+
+    public function delete($entity) {
+
+    }
+
+    public function access($entity) {
+
+    }
+}
