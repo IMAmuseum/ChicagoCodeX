c587969d5a24929226286fa65058bc83800196a1
karnold <karnold@imamuseum.org>
2011-02-28 21:55:35 +0000

    git-svn-id: http://athena.imamuseum.org/svn/ima/trunk/labs/osci/default@16186 a4814893-3c26-0410-bcaf-a94346362ac2

--- modules/citation/includes/CitationEntityController.class.php
+++ modules/citation/includes/CitationEntityController.class.php
@@ -5,24 +5,91 @@
  */
 class CitationEntityController extends DrupalDefaultEntityController {
 
+    public function access($op, $cid) {
+        // TODO: implement
+        if (user_access('administer citations')) return TRUE;
+        switch($op) {
+            case 'view':
+            case 'edit':
+            case 'create':
+        }
+        return TRUE;
+    }
+
     public function save($citation) {
-        $primary_key = (!empty($citation->cid)) 'cid' : null;
-        drupal_write_record('citation', $citation, $primary_key);
+        $transaction = db_transaction();
+        $citation = (object)$citation;
+        field_attach_presave('citation', $citation);
+
+        try {
+            if ($citation->cid) {
+                drupal_write_record('citation', $citation, 'cid');
+                field_attach_update('citation', $citation);
+            } else {
+                drupal_write_record('citation', $citation);
+                field_attach_insert('citation', $citation);
+            }
+
+            return $citation;
+        }
+        catch (Exemption $e) {
+            $transaction->rollback('citation');
+            watchdog_exception('citation', $e);
+            throw $e;
+        }
+        
     }
 
-    public function load($cid) {
+    public function view($citations, $view_mode = 'full') {
+        global $user;
 
+        foreach ($citations as $cid => $citation) {
+            if (!$this->access($cid)) continue;
+
+            $citations[$cid]->content = array();
+
+            field_attach_prepare_view('citation', array($cid => $citation), $view_mode);
+            entity_prepare_view('citation', array($cid => $citation));
+            $citations[$cid]->content += field_attach_view('citation', $citation, $view_mode);
+            $citations[$cid]->content += array(
+                '#theme'        => 'citation',
+                '#citation'     => $citations[$cid],
+                '#view_mode'    => $view_mode,
+                '#children'     => field_attach_view('citation', $citation, $view_mode),
+            );
+
+        }
+        return $citations;
     }
 
-    public function view($citation) {
+    public function build($citations) {
+        $citations = $this->view($citations);
+        $output = '';
 
+        foreach($citations as $cid => $citation) {
+dpm($citation->content);
+             $output .= drupal_render($citation->content);
+        }
+        
+        return $output;
     }
 
     public function delete($cid) {
+        $citation = citation_load($cid);
 
+        if ($citation) {
+            $transaction = db_transaction();
+
+            try {
+                db_delete('citation')->condition('cid', $cid, 'IN')->execute();
+                field_attach_delete('citation', $citation);
+            }
+            catch (Exception $e) {
+                $transaction->rollback();
+                watchdog_exception('citation', $e);
+                throw $e;
+            }
+        }
     }
 
-    public function access($citation) {
-
-    }
 }
