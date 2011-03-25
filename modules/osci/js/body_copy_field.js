be85a511f8f1b77e4a276df91557048fd9ad7ae7
grayb <gray.bowman@gmail.com>
2011-03-25 15:51:20 +0000

    git-svn-id: http://athena.imamuseum.org/svn/ima/trunk/labs/osci/default@16520 a4814893-3c26-0410-bcaf-a94346362ac2

--- modules/osci/js/body_copy_field.js
+++ modules/osci/js/body_copy_field.js
@@ -1,14 +1,67 @@
+
+
 (function ($) {
+	
+	function getPreviewDiv(id, target) {
+		// send nid to server to fetch preview
+		$.get(Drupal.settings.baseUrl + 'ajax/figurepreview/' + id,
+			function (data) {
+				$('.figure_reference_preview', 
+					$(target).parents(".fieldset-wrapper:first")).html(data.div);
+			},
+			"json"
+		);
+	}
+	
+	
+	
     $(document).ready(function() {
-        var config = Drupal.settings.wysiwyg.configs.ckeditor.formatfootnote;
-        $('.footnotes-wrapper .fieldset-wrapper').tabs({
-            selected : 1
+    	var config = Drupal.settings.wysiwyg.configs.ckeditor.formatfootnote;
+
+    	function initCKeditor(obj) {
+    		var obj = $(obj);
+    		var wrapper = obj.parents('.footnotes-wrapper');
+    		var textareas = $('textarea', wrapper);
+    		textareas.each(function() {
+    			// Find all editor instances and destroy them
+                var instance = $(this).data('ckeditorInstance');
+                if (instance !== undefined && instance.destroy && $.isFunction(instance.destroy)) {
+                    instance.destroy();
+                }
+    		});
+            obj.ckeditor(function() {}, config);
+        }
+    	
+        
+        /**************************************************
+         * CKEDITOR 
+         */
+        $('.ui-tabs').live('tabsselect', function(e, ui) {
+            initCKeditor(ui.tab.hash + ' textarea');
+            var field = $('.figure_reference_field', ui.tab.hash);
+            var val = field.val();
+            if (val == parseInt(val)) {
+            	getPreviewDiv(val, field);
+            }
+        });
+        
+        
+        var footWrapper = $('.footnotes-wrapper .fieldset-wrapper').tabs();
+        footWrapper.each(function() {
+        	var wrapper = $(this);
+        	if (wrapper.tabs("length") > 1) {
+        		wrapper.tabs("select", 1);
+        	}
         });
 
-        $('.figures-wrapper .fieldset-wrapper').tabs({
-            selected : 1
+        var figureWrapper = $('.figures-wrapper .fieldset-wrapper').tabs();
+        figureWrapper.each(function() {
+        	var wrapper = $(this);
+        	if (wrapper.tabs("length") > 1) {
+        		wrapper.tabs("select", 1);
+        	}
         });
-
+        
         $("<a />", {
             "class" : "footnote-remove",
             text : "remove",
@@ -65,48 +118,33 @@
             }
         });
 
-        /**************************************************
-         * CKEDITOR 
-         */
-        $('.footnotes-wrapper textarea.first').ckeditor(function() {}, config);
-        $('.ui-tabs').live('tabsselect', function(e, ui) {
-            initCKeditor(ui.tab.hash + ' textarea');
-        });
 
-        function initCKeditor(obj) {
-            $('.footnotes-wrapper textarea').each(function() {
-                // Find all editor instances and destroy them
-                var instance = $(this).data('ckeditorInstance');
-                if (instance != undefined) {
-                    var editor = $(this).ckeditorGet();
-                    editor.destroy();
-                }
-            });
-            $(obj).ckeditor(function() {}, config);
-        }
+
+        
         
         /**************************************************
          * Figure Preview
          */
-        $('.figure_reference_field').live('keyup', function(event) {
-        	// wait a second to see if anything else was pressed
-        	var origVal = event.target.value;
-        	setTimeout(function() {
-        		var currentVal = event.target.value;
-        		if (currentVal == origVal && currentVal != "" && currentVal == parseInt(currentVal)) {
-            		console.log(currentVal, 'retrieving div for id');
-            		// send nid to server to fetch preview
-            		$.get(Drupal.settings.baseUrl + 'ajax/figurepreview/' + currentVal,
-            			function (data) {
-            				console.log(data, 'retrieved data');
-            				console.log(event.target);
-            				$('.figure_reference_preview', 
-            					$(event.target).parents(".fieldset-wrapper:first")).html(data.div);
-            			},
-            			"json"
-            		);
-        		}
-        	}, 1250);
+        $('.figure_reference_field').live({
+        	'keyup': function(event) {
+	        	// wait a second to see if anything else was pressed
+	        	var origVal = event.target.value;
+	        	setTimeout(function() {
+	        		var currentVal = event.target.value;
+	        		if (currentVal == origVal && currentVal != "" && currentVal == parseInt(currentVal)) {
+	            		getPreviewDiv(currentVal, event.target);
+	        		}
+	        	}, 1250);
+        	},
+	        'blur': function(event) {
+	        	setTimeout( function() {
+	        		var currentVal = event.target.value;
+	            	if (currentVal == parseInt(currentVal)) {
+	            		getPreviewDiv(currentVal, event.target);
+	            	}
+	
+	        	}, 500);
+	        }
         });
     });
 })(jQuery);
