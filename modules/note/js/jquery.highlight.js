b20234f65387903120d417fdda177328729487f9
karnold <karnold@imamuseum.org>
2011-03-25 20:35:29 +0000

    added highlight plugin
    git-svn-id: http://athena.imamuseum.org/svn/ima/trunk/labs/osci/default@16565 a4814893-3c26-0410-bcaf-a94346362ac2

---
+++ modules/note/js/jquery.highlight.js
@@ -0,0 +1,128 @@
+(function ($) {
+
+	$.fn.highlight = function(options) { 
+		var settings = $.extend({
+			wrapperElement: 'span',
+			wrapperClass: 'highlight',
+		}, options);
+		
+		// Listen for mouse up event, capture text, and highlight it
+	    $(this).mouseup(function() {
+	        var selectionRange = getSelected();
+	        var parentNode = selectionRange.commonAncestorContainer;
+	        var foundStart = false;
+	        var foundEnd = false;
+
+	        /* 
+	         * A single node will have itself as the common ancestor.
+	         */
+	        if (parentNode.isSameNode(this)) {
+	        	/*
+	        	 * nodeType 1 = textNode
+	        	 * nodeType 3 = htmlNode
+	        	 */
+	        	if (this.nodeType == 1) { 
+		        	processTxtNode(this, 'start', selectionRange.startOffset);
+		        	processTxtNode(this, 'end', selectionRange.endOffset);
+	        	} else if (this.nodeType == 3) {
+	        		processHtmlNode(this, 'start', selectionRange.startOffest);
+	        		processHtmlNode(this, 'end', selectionRange.endOffset);
+	        	}
+		    } else { 
+		        $(parentNode).contents().each(function() {
+		            // Text Nodes
+		            if (selectionRange.startContainer.isSameNode(this) && !foundStart) {
+		            	processTxtNode(this, 'start', selectionRange.startOffset);
+		                foundStart = true;
+		            } else if (selectionRange.endContainer.isSameNode(this) && !foundEnd) {
+		            	processTxtNode(this, 'end', selectionRange.endOffset);
+		                foundEnd = true;
+		            } else if (this.nodeType == 3 && foundStart == true && foundEnd !== true) {
+		            	processTxtNode(this, 'middle');
+		            }
+	
+		            // HTML Nodes
+		            if (selectionRange.startContainer.parentElement.isSameNode(this) && !foundStart) {
+		            	processHtmlNode(this, 'start', selectionRange.startOffset)
+		                foundStart = true;
+		            } else if (selectionRange.endContainer.parentElement.isSameNode(this) && !foundEnd) {
+		            	processHtmlNode(this, 'end', selectionRange.endOffset);
+		                foundEnd = true;
+		            } else if (this.nodeType == 1 && foundStart == true && foundEnd !== true) {
+		            	processHtmlNode(this, 'middle');
+		            }
+		        });
+		    }
+	    });
+	    
+	    // Get selected text and return the selection range object
+	    
+	    var getSelected = function() {
+	        if (window.getSelection) { // W3C compliant browser.  aka, not IE
+	            var selection = window.getSelection();
+	        } else { // IE
+	            var selection = document.selection && document.selection.createRange();
+	        }
+	        
+	        if (selection == '') return false;
+
+	        if (selection.getRangeAt)
+	            var range = selection.getRangeAt(0);
+	        else { // Safari!
+	            var range = document.createRange();
+	            range.setStart(selection.anchorNode,selection.anchorOffset);
+	            range.setEnd(selection.focusNode,selection.focusOffset);
+	        }
+
+	        return range;
+	    }
+	    
+	    // Process a text node with a wrapper
+	    var processTxtNode = function(textNode, position, offset) {
+	        var wrapper = document.createElement(this.settings.wrapperElement);
+	        wrapper.className = this.settings.wrapperClass;
+
+	    	switch(position) {
+	    		case 'start':
+	    			var preText = document.createTextNode(textNode.nodeValue.substring(0, offset));
+	    			wrapper.appendChild(document.createTextNode(textNode.nodeValue.substring(offset, textNode.length)));
+	    			textNode.parentNode.insertBefore(subText, textNode);
+	    	        textNode.parentNode.replaceChild(wrapper, textNode);
+	    			break;
+	    		case 'middle':
+	    			wrapper.appendChild(document.createTextNode(textNode.nodeValue));
+	    	        textNode.parentNode.replaceChild(wrapper, textNode);
+	    			break;
+	    		case 'end':
+	    			wrapper.appendChild(document.createTextNode(textNode.nodeValue.substring(0, offset)));
+	    			var postText = document.createTextNode(textNode.nodeValue.substring(offset, textNode.length));
+	    			textNode.parentNode.insertBefore(wrapper, textNode);
+	    	        textNode.parentNode.replaceChild(postText, textNode);
+	    			break;
+	    	}
+	    	
+	    }
+
+	    // Process html node with a wrapper
+	    var processHtmlNode = function(htmlNode, position, offset) {
+	    	switch(position) {
+	    		case 'start':
+	    			var newHtml = $(htmlNode).html().substring(0, offset) 
+	                	+ '<span>' 
+	                	+ $(htmlNode).html().substring(offset, htmlNode.length) 
+	                	+ '</span>';
+	    			$(htmlNode).html(newHtml);
+	    			break;
+	    		case 'middle':
+	                $(htmlNode).html('<span>' + $(nodeHtml).html() + '</span>');
+	    			break;
+	    		case 'end':
+	    			var newHtml = '<span>'
+	                    + $(htmlNode).html().substring(0, offset) 
+	                    + '</span>'
+	                    + $(htmlNode).html().substring(offset, htmlNode.length);
+	                $(htmlNode).html(newHtml);
+	    			break;
+	    	}
+	    }
+}(jQuery);
