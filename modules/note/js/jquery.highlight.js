d498d5b0637b3310bdb4762b3f5989eccdd82c96
karnold <karnold@imamuseum.org>
2011-05-10 14:00:30 +0000

    git-svn-id: http://athena.imamuseum.org/svn/ima/trunk/labs/osci/default@17017 a4814893-3c26-0410-bcaf-a94346362ac2

--- modules/note/js/jquery.highlight.js
+++ modules/note/js/jquery.highlight.js
@@ -33,37 +33,40 @@
 
             $(parentNode).contents().each(function() {
                 if ($(this).text() == '') return;
-
-                var isStartNode = properties.startNode.indexOf($(this).text());
-                var isEndNode   = properties.endNode.indexOf($(this).text());
-                var startOffset = 0;
-                var endOffset   = $(this).text().length;
-
-                // Find the start node
-                if (isStartNode == 0) {
-                    startOffset = properties.startOffset;
-                    processNode = true;
-                }
-
-                // Find the end node
-                if (isEndNode == 0) {
-                    endOffset = properties.endOffset;
-                    var foundEnd = true;
-                }
-
-                if (this.nodeType == 1 && processNode === true) { // HTML
-                    processHtmlNode(this, startOffset, endOffset);
-                } else if (this.nodeType == 3 && processNode === true) { // Text
-                    processTxtNode(this, startOffset, endOffset);
-                }
-
-                if (foundEnd === true) processNode = false; 
                 
+                highlightNode(this, properties, processNode);
             });
 console.log(processNode);
 
             settings.onSelection(this, e, properties);
 	    });
+	    
+	    var highlightNode = function(obj, properties, processNode) {
+            var isStartNode = properties.startNode.indexOf($(this).text());
+            var isEndNode   = properties.endNode.indexOf($(this).text());
+            var startOffset = 0;
+            var endOffset   = $(obj).text().length;
+
+            // Find the start node
+            if (isStartNode == 0) {
+                startOffset = properties.startOffset;
+                processNode = true;
+            }
+
+            // Find the end node
+            if (isEndNode == 0) {
+                endOffset = properties.endOffset;
+                var foundEnd = true;
+            }
+
+            if (this.nodeType == 1 && processNode === true) { // HTML
+                processHtmlNode(obj, startOffset, endOffset);
+            } else if (this.nodeType == 3 && processNode === true) { // Text
+                processTxtNode(obj, startOffset, endOffset);
+            }
+
+            if (foundEnd === true) processNode = false;
+	    }
 	    
 	    // Get selected text and return the selection range object
 	    var getSelected = function() {
