563ae5315f7d99f25b9916efb21ffde380225a3e
kjaebker <kjaebker@gmail.com>
2011-05-19 18:36:50 +0000

    updating overscroll plugin
    git-svn-id: http://athena.imamuseum.org/svn/ima/trunk/labs/osci/default@17116 a4814893-3c26-0410-bcaf-a94346362ac2

--- modules/osci/js/jquery.overscroll.js
+++ modules/osci/js/jquery.overscroll.js
@@ -1,5 +1,5 @@
 /*!
- * Overscroll v1.4.2
+ * Overscroll v1.4.3
  *  A jQuery Plugin that emulates the iPhone scrolling experience in a browser.
  *  http://azoffdesign.com/overscroll
  *
@@ -13,7 +13,7 @@
  * For API documentation, see the README file
  *  https://github.com/azoff/Overscroll/blob/master/README.md
  *
- * Date: Thursday, February 17th 2011
+ * Date: Saturday, April 23rd 2011
  */
 
 /*jslint onevar: true, strict: true */
@@ -59,14 +59,8 @@
 		},
 
         checkIosDevice: function() {
-            if (o.isIOS === undefined) {
-                var devices = ["iPhone", "iPad", "iPod"], i;
-                for (i=0; i<devices.length; i++) {
-                    if (navigator.platform.indexOf(devices[i]) >= 0) {
-                        return (o.isIOS = true);
-                    }
-                }
-                return (o.isIOS = false);
+            if (o.isIOS === undefined) {				
+                o.isIOS = /iP((hone)|(ad)|(od))/.test(navigator.platform);
             }
             return o.isIOS;
         },
@@ -83,8 +77,7 @@
 				wheelDelta: o.constants.wheelDelta,
 				scrollDelta: o.constants.scrollDelta,
 				direction: 'multi',
-				cancelOn: '',
-				onDriftEnd: $.noop
+				cancelOn: ''
 			}, (options || {}));
 			
 			options.scrollDelta = m.abs(options.scrollDelta);
@@ -108,7 +101,7 @@
 					data.thumbs.horizontal = $(o.div).css(o.getThumbCss(data.sizing.thumbs.horizontal)).fadeTo(0, 0);
 					target.prepend(data.thumbs.horizontal);	
 				}
-
+				
 				if(data.sizing.container.scrollHeight > 0 && options.direction !== 'horizontal') {
 					data.thumbs.vertical = $(o.div).css(o.getThumbCss(data.sizing.thumbs.vertical)).fadeTo(0, 0);
 					target.prepend(data.thumbs.vertical);				
@@ -119,6 +112,10 @@
 			data.target = target;
 			data.options = options;
 				
+		},
+		
+		triggerEvent: function(event, data) {
+			data.target.trigger('overscroll:' + event);
 		},
 		
 		// toggles the drag mode of the target
@@ -187,7 +184,6 @@
 		        event.data.wheelCapture = undefined;
 		        o.toggleThumbs(event.data, false);
 		        event.data.target.data('dragging', false);
-		        event.data.options.onDriftEnd.call(event.data.target, event.data);
 		    }, o.constants.timeout);
 		
 			return false;
@@ -222,13 +218,13 @@
 		start: function(event) {
 
             o.clearInterval();
-            
 
             if (!$(event.target).is(event.data.options.cancelOn)) {               
                 o.normalizeEvent(event);
                 event.data.target.bind(o.events.drag, event.data, o.drag).stop(true, true).data('dragging', false);
                 event.data.position = o.setPosition(event, {});
                 event.data.capture = o.setPosition(event, {}, 2);
+				o.triggerEvent('dragstart', event.data);
                 return false;
             }
 			
@@ -279,11 +275,12 @@
 
 				event.data.target.unbind(o.events.drag, o.drag);
 				
+				o.triggerEvent('dragend', event.data);
+				
 				if(event.data.target.data('dragging')) {
 				 
 				    o.drift(this, event, function(data){
                         data.target.data('dragging', false);
-                        data.options.onDriftEnd.call(data.target, data);
                         o.toggleThumbs(data, false);
                     });
 
@@ -326,6 +323,8 @@
                 scrollTop -= dy;
             }
 
+			o.triggerEvent('driftstart', event.data);
+
             o.setInterval(w.setInterval(function() {
 
                 var done = true, min = 1, max = -1;
@@ -348,6 +347,7 @@
 
                 if (done) {
                     o.clearInterval();
+					o.triggerEvent('driftend', event.data);
                     callback.call(null, event.data);
                 }
 
