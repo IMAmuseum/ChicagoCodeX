549a37eee678bf3a109994cc8f90f1536d45d59a
grayb <gray.bowman@gmail.com>
2011-05-27 14:44:15 +0000

    iip touch problems
    git-svn-id: http://athena.imamuseum.org/svn/ima/trunk/labs/osci/default@17186 a4814893-3c26-0410-bcaf-a94346362ac2

--- modules/osci/js/osci_iip_polymaps.js
+++ modules/osci/js/osci_iip_polymaps.js
@@ -1870,7 +1870,7 @@ po.touch = function() {
       angle,
       locations = {}; // touch identifier -> location
 
-  window.addEventListener("touchmove", touchmove, false);
+  container.addEventListener("touchmove", touchmove, false);
 
   function touchstart(e) {
     var i = -1,
