e39ca44f60dfcd0c8631f9550af56ad805c1d6d4
ebachta <ebachta@imamuseum.org>
2011-11-08 15:57:52 +0000

    updated to fix ie error
    git-svn-id: http://athena.imamuseum.org/svn/ima/trunk/labs/osci/default@17660 a4814893-3c26-0410-bcaf-a94346362ac2

--- modules/osci/js/polymaps.js
+++ modules/osci/js/polymaps.js
@@ -493,7 +493,7 @@ po.map = function() {
 
   map.focusableParent = function() {
     for (var p = container; p; p = p.parentNode) {
-      	if (p.className.match) {
+      	if (p.className && p.className.match) {
 			if (p.className.match(/conservation-asset/)) return p;
 		}
     }
