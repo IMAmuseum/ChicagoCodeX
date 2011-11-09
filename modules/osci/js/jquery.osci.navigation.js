3ee5b39a2289df04820d6e5fbecac6f02edfd279
kjaebker <kjaebker@gmail.com>
2011-11-09 15:16:57 +0000

    fix re-layout when fullscreen options clicked
    git-svn-id: http://athena.imamuseum.org/svn/ima/trunk/labs/osci/default@17670 a4814893-3c26-0410-bcaf-a94346362ac2

--- modules/osci/js/jquery.osci.navigation.js
+++ modules/osci/js/jquery.osci.navigation.js
@@ -177,6 +177,10 @@
         //function to call when the browser is resized
         function _osci_resize()
         {
+            if ($("#ca-ui-fullscreen-modal").length || $("#modalBackdrop").length) {
+                return;
+            }
+            
         	//get the first paragraph currently displayed so we can try to stay on the same page after the resize
             var firstParagraph = $("div.osci_page_" + (base.data.currentPage + 1)).find("p.osci_paragraph:first");
             base.data.to = { operation : "paragraph", value : firstParagraph.data("paragraph_id")};
