2ae2ef1776c6c807d62a319e4b420b124a0cf5d8
Gray Bowman <gray.bowman@gmail.com>
2012-07-10 12:07:44 -0400

    content bar wrapper and handle events

--- frontend/js/backbone/views/ContentBarView.js
+++ frontend/js/backbone/views/ContentBarView.js
@@ -4,12 +4,32 @@ if (typeof Aic.views === 'undefined'){Aic.views = {};}
 // Aic Namespace Initialization //
 
 Aic.views.ContentBar = OsciTk.views.BaseView.extend({
-	id: 'content-bar',
+	id: 'content-bar-wrapper',
 	template: OsciTk.templateManager.get('content-bar'),
 	initialize: function() {
-		this.render();
+		this.isOpen = false;
+		
+		app.dispatcher.on('figuresLoaded', function(figures) {
+			console.log('draw a figures tab');
+		}, this);
+
+		app.dispatcher.on('footnotesLoaded', function(footnotes) {
+			console.log('draw a footnotes tab');
+		}, this);
+
+		//this.render();
 	},
 	render: function() {
+		this.$el.show();
 		this.$el.html(this.template());
+
+		// bind handle to toggle drawer
+		this.$el.find('#content-bar-handle').on('click', this, function(event) {
+			var $this = event.data;
+			$this.toggleDrawer();
+		});
+	},
+	toggleDrawer: function() {
+		
 	}
 });
