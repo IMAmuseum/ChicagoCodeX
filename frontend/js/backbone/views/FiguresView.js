3054f4a36fdaa82a9efd450dfd7e7a9728dcb39a
Gray Bowman <gray.bowman@gmail.com>
2012-07-24 15:21:51 -0400

    Figures bar now functioning and populated

--- frontend/js/backbone/views/FiguresView.js
+++ frontend/js/backbone/views/FiguresView.js
@@ -8,44 +8,98 @@ Aic.views.Figures = OsciTk.views.BaseView.extend({
 	template: OsciTk.templateManager.get('figures'),
 	initialize: function() {
 		this.isOpen = false;
+		this.collection = app.collections.figures;
+		this.page = 1;
+		this.maxPage = 1;
 
-		// draw the footnotes ui only if footnotes become available
-		app.dispatcher.on('footnotesLoaded', function(footnotes)
-		{
+		// draw the figures ui only if figures become available
+		app.dispatcher.on('figuresLoaded', function(figures) {
 			this.render();
+		}, this);
 
-			// close the drawer when requested
-			app.dispatcher.on('drawersClose', function(caller) {
-				if (caller !== this) {
-					this.closeDrawer();
+		// close the drawer when requested
+		app.dispatcher.on('drawersClose', function(caller) {
+			if (caller !== this) {
+				this.closeDrawer();
+			}
+		}, this);
+
+		// move the drawer handle when a UI shift happens
+		app.dispatcher.on('uiShift', function(params) {
+			if (params.caller != this) {
+				if (typeof(params.x) !== 'undefined') {
+					// move the content bar handle
+					var handle = this.$el.find('#figures-handle');
+					var left = parseInt(handle.css('left'), 10);
+					handle.animate({
+						left: (left + params.x) + 'px'
+					});
 				}
-			}, this);
-
-			// move the drawer handle when a UI shift happens
-			app.dispatcher.on('uiShift', function(params) {
-				if (params.caller != this) {
-					if (typeof(params.x) !== 'undefined') {
-						// move the content bar handle
-						var handle = this.$el.find('#footnotes-handle');
-						var left = parseInt(handle.css('left'), 10);
-						handle.animate({
-							left: (left + params.x) + 'px'
-						});
-					}
-				}
-			}, this);
-
-			// bind handle to toggle drawer
-			this.$el.find('#footnotes-handle').on('click', this, function(event) {
-				console.log(event, 'event');
-				var $this = event.data;
-				$this.toggleDrawer();
-			});
+			}
 		}, this);
 	},
 	render: function() {
 		this.$el.css('display', 'block');
-		this.$el.html(this.template());
+		var data = { figures: this.collection.models };
+		this.$el.html(this.template(data));
+
+		// set figures list width
+		var itemWidth = this.$el.find('#figures-list li').outerWidth();
+		var itemCount = this.collection.length;
+		this.$el.find('#figures-list').width(itemWidth * itemCount);
+		
+		// justify contents across container width
+		var containerWidth = this.$el.find('#figures-list-container').width();
+		var numToFit = Math.floor(containerWidth / itemWidth);
+		var paddingToAdd = (containerWidth - (numToFit * itemWidth)) / numToFit;
+		var currentPadding = parseInt(this.$el.find('#figures-list li').css('padding-left'), 10) * 2;
+		var newPadding = (currentPadding + paddingToAdd) / 2;
+		this.$el.find('#figures-list li').css({
+			'padding-left': newPadding + 'px',
+			'padding-right': newPadding + 'px'
+		});
+
+		// css has changed, get item width again
+		itemWidth = this.$el.find('#figures-list li').outerWidth();
+		
+		// reset list length now that padding has been added
+		this.$el.find('#figures-list').width(itemWidth * itemCount);
+
+		// set the max page value
+		this.maxPage = Math.ceil((itemWidth * itemCount) / containerWidth);
+		
+		// bind handle to toggle drawer
+		this.$el.find('#figures-handle').on('click', this, function(event) {
+			var $this = event.data;
+			$this.toggleDrawer();
+		});
+
+		// bind previous button
+		this.$el.find('#figures-nav-prev .figures-indicator').on('click', this, function(event) {
+			var $this = event.data;
+			if ($this.page > 1) {
+				$this.page--;
+				$this.translateToPage();
+			}
+		});
+
+		// bind next button
+		this.$el.find('#figures-nav-next .figures-indicator').on('click', this, function(event) {
+			var $this = event.data;
+			if ($this.page < $this.maxPage) {
+				$this.page++;
+				$this.translateToPage();
+			}
+		});
+	},
+	translateToPage: function() {
+		var width = this.$el.find('#figures-list-container').width();
+		var list = this.$el.find('#figures-list');
+		var pos = -(width * (this.page - 1));
+
+		list.css({
+			'-webkit-transform': 'translate3d(' + pos + 'px, 0px, 0px)'
+		});
 	},
 	toggleDrawer: function() {
 		if (this.isOpen) {
@@ -60,108 +114,15 @@ Aic.views.Figures = OsciTk.views.BaseView.extend({
 	openDrawer: function() {
 		// tell other drawers to close
 		app.dispatcher.trigger('drawersClose', this);
-		this.$el.animate({
+		this.$el.find('#figures-content').animate({
 			height: '300px'
-		},
-		{
-			step: function() {
-				$(this).css("overflow","visible");
-			},
-			complete: function() {
-				$(this).css("overflow","visible");
-			}
 		});
 		this.isOpen = true;
 	},
 	closeDrawer: function() {
-		this.$el.animate({
+		this.$el.find('#figures-content').animate({
 			height: '0'
-		},
-		{
-			step: function() {
-				$(this).css("overflow","visible");
-			},
-			complete: function() {
-				$(this).css("overflow","visible");
-			}
 		});
 		this.isOpen = false;
 	}
-	// className: 'figures-view',
-	// template: OsciTk.templateManager.get('figures'),
-	// initialize: function() {
-	// 	// re-render this view when collection changes
-	// 	app.collections.figures.on('add remove reset', function() {
-	// 		console.log('rendering');	
-	// 		this.render();
-	// 	}, this);
-	// },
-	// events: {
-	// 	"click .figure-preview": "onFigurePreviewClicked",
-	// 	"click a.view-in-context": "onViewInContextClicked"
-	// },
-	// onFigurePreviewClicked: function(event_data) {
-	// 	app.dispatcher.trigger('showFigureFullscreen', $(event_data.target).parent('figure').attr('data-figure-id'));
-	// 	return false;
-	// },
-	// onViewInContextClicked: function(event_data) {
-	// 	app.dispatcher.trigger('navigate', { identifier: $(event_data.target).parent('figure').attr('data-figure-id') });
-	// 	app.views.toolbarView.contentClose();
-	// 	return false;
-	// },
-	// active: function() {
-	// 	var fig_data = app.collections.figures.toJSON();
-	// 	// Set the width of the figure reel if there is more than one thumbnail
-	// 	if (fig_data.length > 1) {
-	// 		var thumbs = this.$el.find('figure.thumbnail');
-	// 		this.$el.find('.figure-browser .figure-reel').width(thumbs.length * (thumbs.outerWidth(true)));
-	// 	}
-	// },
-	// render: function() {
-	// 	var fig_data = app.collections.figures.toJSON();
-	// 	this.$el.html(this.template({figures: fig_data}));
-
-	// 	// When the reader clicks on a figure thumbnail, show the preview for that figure...
-	// 	this.$el.on('click', 'figure.thumbnail', {view: this},function(e) {
-	// 		e.data.view.$el.find('.figure-browser').hide();
-	// 		e.data.view.$el.find('.figure-previews figure.active').hide().removeClass('active');
-	// 		var content = e.data.view.$el.find("figure.preview[data-figure-id='" + $(this).attr('data-figure-id') + "']");
-	// 		content.show().addClass('active');
-	// 		e.data.view.displayTitle();
-	// 		e.data.view.$el.find('.figure-previews').show();
-	// 		app.views.toolbarView.updateHeight();
-	// 	});
-
-	// 	// When going back to the grid, hide the current preview and replace the close button
-	// 	this.$el.on('click', '.back-to-grid', {view: this}, function(e) {
-	// 		e.data.view.$el.find('.figure-previews').hide();
-	// 		e.data.view.$el.find('.figure-browser').show();
-	// 		app.views.toolbarView.updateHeight();
-	// 	});
-
-	// 	this.$el.on('click', '.figure-nav.next', {view: this}, function(e) {
-	// 		var new_fig = e.data.view.$el.find('figure.preview.active').hide().removeClass('active').next('figure.preview');
-	// 		if (new_fig.length === 0) {
-	// 			new_fig = e.data.view.$el.find('figure.preview').first();
-	// 		}
-	// 		new_fig.show().addClass('active');
-	// 		e.data.view.displayTitle();
-	// 	});
-
-	// 	this.$el.on('click', '.figure-nav.prev', {view: this}, function(e) {
-	// 		var new_fig = e.data.view.$el.find('figure.preview.active').hide().removeClass('active').prev('figure.preview');
-	// 		if (new_fig.length === 0) {
-	// 			new_fig = e.data.view.$el.find('figure.preview').last();
-	// 		}
-	// 		new_fig.show().addClass('active');
-	// 		e.data.view.displayTitle();
-	// 	});
-
-	// 	return this;
-	// },
-	// displayTitle: function() {
-	// 	var id = this.$el.find('figure.preview.active').attr('data-figure-id');
-	// 	var figure = app.collections.figures.get(id);
-	// 	this.$el.find('h2 span.title').html(figure.get('title'));
-	// }
 });
