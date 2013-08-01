2d6e3c043fb05334fa74a4fb7d62bdfd0e9d4096
Kyle Jaebker <kjaebker@gmail.com>
2013-08-01 14:28:56 -0400

    execute a search when form is submitted

--- frontend/js/oscitk/views/SearchView.js
+++ frontend/js/oscitk/views/SearchView.js
@@ -43,6 +43,53 @@ OsciTk.views.Search = OsciTk.views.BaseView.extend({
 	},
 	search: function() {
 		if (!this.$el.is(":visible")) this.$el.show();
+
+		var that = this;
+		// set keyword
+		this.query.keyword = this.$el.find('.search-keyword').val();
+		// reset collection
+		this.response.docs.reset();
+		// let the template know that we can now display results
+		this.hasSearched = true;
+
+		// build query params to send to api
+		var queryParams = {
+			key: this.query.keyword,
+			group: 'true',
+			page: this.query.page,
+			sort: this.query.sort
+		};
+
+		var publicationId = 'pid:' + app.models.docPackage.get('id');
+		// check if publication filter already exists
+		if (_.indexOf(this.query.filters, publicationId) === -1) {
+			// filter by publication id
+			this.query.filters.push(publicationId);
+		}
+
+		if (this.query.filters.length) {
+			queryParams['filters'] = this.query.filters.join(' ');
+		}
+
+		// send search query
+		$.ajax({
+			url: app.config.get('endpoints')['OsciTkSearch'],
+			data: queryParams,
+			success: function(data) {
+				data = JSON.parse(data);
+				// add the incoming docs to the results collection
+				that.response.docs.reset(data.docs);
+				that.response.facets = data.facets;
+				that.response.numFound = data.numFound;
+				// re-render the search view
+				//that.renderResults();
+				// handle container resizing
+				//that.resizeContainers();
+			},
+			error: function() {
+				// error handling
+			}
+		});
 	},
 	toggleResultsType: function(e) {
 		e.preventDefault();
