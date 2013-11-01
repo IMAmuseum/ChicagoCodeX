c66f593ad37d2aca5799380ccdbc25bdef22649f
Gray Bowman <gray.bowman@gmail.com>
2013-11-01 11:57:19 -0400

    added display name field for glossary terms, and custom glossary doc rendering

---
+++ modules/aic_custom/templates/aic_custom_glossary_document.tpl.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head></head>
+    <body>
+        <dl>
+        <?php foreach ($terms as $name => $term): ?>
+        	<dt data-tid="<?php print $term['term_id']; ?>"><dfn><?php print $term['display_name']; ?></dfn></dt>
+        	<dd><?php print $term['definition']; ?></dd>
+        <?php endforeach; ?>
+        </dl>
+    </body>
+</html>
