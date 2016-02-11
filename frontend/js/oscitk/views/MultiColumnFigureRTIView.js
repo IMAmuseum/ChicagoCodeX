34751875f9b7758fc7289a0c32b04b369ef6b31e
surreal8 <tinashah8@gmail.com>
2016-02-11 17:30:21 -0600

    adding fullscreen for drawer images

--- frontend/js/oscitk/views/MultiColumnFigureRTIView.js
+++ frontend/js/oscitk/views/MultiColumnFigureRTIView.js
@@ -3,12 +3,26 @@ OsciTk.views.figureTypeRegistry["rti_viewer"] = "MultiColumnFigureRTI";
 OsciTk.views.MultiColumnFigureRTI = OsciTk.views.MultiColumnFigure.extend({
 	renderContent: function() {
 		var container = this.$el.find(".figure_content");
-		var containerHeight = container.height();
-		var containerWidth = container.width();
 		container.html(this.model.get("content"));
 		this.contentRendered = true;
 	},
 	fullscreen:function() {
-		return null;
+		var content = this.model.get("content");
+		content = $(content);
+		var windowWidth = $(window).width() - 200;
+	    var windowHeight = $(window).height() - 200;
+	    var canvasFullscreenName = content.data('rti-name') + '_fullscreen';
+	    var fullscreenCode = '<div class="figure_content" style="width:'+ windowWidth+'px; height: '+windowHeight+'px;"><div class="rtiViewer" data-rti-div = "rtiViewer" data-rti-name = "'+canvasFullscreenName+'" data-rti-url = "'+content.data('rti-url')+'" data-rti-width = "'+content.data('rti-width')+'" data-rti-height = ""'+content.data('rti-height')+'"" ></div><script>createRtiViewer(true);</script></div>';
+		$.fancybox.open({
+		  padding: 0,
+		  scrolling: 'no',
+          content: fullscreenCode,
+		  title: this.model.get('caption'),
+		    helpers: {
+			  title: {
+			    type : 'inside'
+				}
+			}
+        });
 	}
 });
